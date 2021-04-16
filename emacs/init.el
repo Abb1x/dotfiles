374cff794244562cecdcae9e01d40467ceb495f7
Abb1x <abb1x@protonmail.com>
2021-04-16 11:46:00 -0400

    emacs

--- emacs/init.el
+++ emacs/init.el
@@ -169,13 +169,13 @@
   :custom
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
-(defun efs/org-mode-visual-fill ()
+(defun abx/org-mode-visual-fill ()
   (setq visual-fill-column-width 100
         visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
 
 (use-package visual-fill-column
-  :hook (org-mode . efs/org-mode-visual-fill))
+  :hook (org-mode . abx/org-mode-visual-fill))
 
 (use-package evil)
 (evil-mode)
